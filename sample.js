1ca7b5b42ef832b9db616bc68e258e164a31a620
murachany <92914178+murachany@users.noreply.github.com>
2021-11-25 17:18:34 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,1 +1,8 @@
-
+<!-- ボタンクリック後の処理 -->
+        //<script language="javascript" type="text/javascript">
+            function clickMessageDisp() {
+                var message = document.getElementById("input-message").value;
+                message = "入力したメッセージ：" + message;
+                document.getElementById("output-message").innerHTML = message;
+            }
+        //</script>
